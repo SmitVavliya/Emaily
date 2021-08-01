f09151aab934e6a7aebbf49ad71681177a1e1ad5
Smit12345 <srvavliya79@gmail.com>
2021-08-01 17:49:12 +0530

    c

--- services/Mailer.js
+++ services/Mailer.js
@@ -26,7 +26,7 @@ class Mailer extends helper.Mail {
 
   addClickTracking() {
     const trackingSettings = new helper.TrackingSettings();
-    const clickTracking = new helper.ClickTracking(true, true);
+    const clickTracking = new helper.ClickTracking(true, true); // (enable, enable_text)
 
     trackingSettings.setClickTracking(clickTracking);
     this.addTrackingSettings(trackingSettings);
